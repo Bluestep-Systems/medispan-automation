74ec29e733332a76bf97fe0c24025cd3c672274c
James Schriever <james.r.schriever@gmail.com>
2022-10-18 10:13:40 -0600

    Fixed missing }

--- installer/scripts/sourced/post-install-hooks-src.sh
+++ installer/scripts/sourced/post-install-hooks-src.sh
@@ -1,4 +1,4 @@
-if [ -d "${M6N_POST_INSTALL_HOOKS_DIR" ]; then
+if [ -d "${M6N_POST_INSTALL_HOOKS_DIR}" ]; then
     echo "Sourcing post-install hooks ..."
     for hook in "${M6N_POST_INSTALL_HOOKS_DIR}"/*.sh; do
         echo "Running post-install hook: ${hook}"
