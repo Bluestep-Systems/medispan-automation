0ee48d395b2caec73e3119b0c810698ba6619a3b
James Schriever <james@bluestep.net>
2025-02-07 16:38:43 -0700

    Enhance docker-postgres-build script to include timestamped image tags Include base build as part of build

--- installer/docker-postgres-build.sh
+++ installer/docker-postgres-build.sh
@@ -4,10 +4,18 @@ set -e
 
 cd $(dirname "$0")
 
+./docker-build.sh ${1}
+
 : ${PGVERSION="42.5.0"}
+NOW=$(date +%Y%m%d%H%M)
 export PGVERSION
-docker build . --build-arg PGVERSION -t caffeine01/medispan-installer:jdbc-postgres -t caffeine01/medispan-installer:jdbc-postgres-${PGVERSION} -f Dockerfile-postgres
+docker build . --build-arg PGVERSION \
+  -t caffeine01/medispan-installer:jdbc-postgres \
+  -t caffeine01/medispan-installer:jdbc-postgres-${PGVERSION} \
+  -t caffeine01/medispan-installer:jdbc-postgres-${NOW} \
+  -f Dockerfile-postgres
 
 [ "--push" == "${1}" ] \
     && docker push caffeine01/medispan-installer:jdbc-postgres \
-    && docker push caffeine01/medispan-installer:jdbc-postgres-${PGVERSION}    
+    && docker push caffeine01/medispan-installer:jdbc-postgres-${PGVERSION} \
+    && docker push caffeine01/medispan-installer:jdbc-postgres-${NOW}
